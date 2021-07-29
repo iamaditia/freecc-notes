3a5eda67985a840ea42b8b3d4807fd7ea906f3fb
aditia <aditia017@gmail.com>
2021-07-29 07:40:23 +0700

    Add problem no. 11

--- JavaScript/BAS.js
+++ JavaScript/BAS.js
@@ -281,8 +281,27 @@ Make sure the rest of the word is in lower case.
 For the purpose of this exercise, you should also capitalize connecting words like the and of.
 */
 
+// Elegant solution from freeCodeCamp using Regex
+
 function titleCase(str) {
-  return str;
+  return str.toLowerCase().replace(/(^|\s)\S/g, L => L.toUpperCase());
 }
 
 titleCase("I'm a little tea pot");
+
+// 12. Slice and Splice
+/*
+You are given two arrays and an index.
+
+Copy each element of the first array into the second array, in order.
+
+Begin inserting elements at index n of the second array.
+
+Return the resulting array. The input arrays should remain the same after the function runs.
+*/
+
+function frankenSplice(arr1, arr2, n) {
+  return arr2;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
