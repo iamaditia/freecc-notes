5855f50d6d2762fa089b6d0564e4e03530a24e92
aditia <aditia017@gmail.com>
2021-08-01 16:16:49 +0700

    Add problem #15

--- JavaScript/BAS.js
+++ JavaScript/BAS.js
@@ -322,8 +322,6 @@ Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
 Hint: Try converting each value to a Boolean.
 */
 
-// My current progress
-
 function bouncer(arr) {
   let newArr = [];
   for (let i = 0; i < arr.length; i++) {
@@ -363,3 +361,54 @@ function getIndexToIns(arr, num) {
 
 
 getIndexToIns([40, 60], 50);
+
+// 15. Mutations
+/*
+Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+For example, ["hello", "Hello"], should return true
+because all of the letters in the second string are present in the first, ignoring case.
+
+The arguments ["hello", "hey"] should return false
+because the string hello does not contain a y.
+
+Lastly, ["Alien", "line"], should return true
+because all of the letters in line are present in Alien.
+*/
+
+// Current solution
+
+function mutation(arr) {
+  let arr2 = arr[0].toLowerCase();
+  let arr3 = arr[1].toLowerCase();
+  let newArr = [];
+  for (let i = 0; i < arr2.length; i++) {
+    for (let j = 0; j < arr3.length; j++) {
+      if (arr2[i] == arr3[j]) {
+        newArr.push(true);
+      }
+    }
+  }
+  if (newArr.length == arr3.length) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// My solution using indexOf
+
+function mutation(arr) {
+  let lowArr0 = arr[0].toLowerCase();
+  let lowArr1 = arr[1].toLowerCase();
+  for (let i = 0; i < lowArr1.length; i ++) {
+    if (lowArr0.indexOf(lowArr1[i]) == -1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+mutation(["hello", "Hello"]);
+
+// freeCodeCamp solution
