28a423f17399c577664afbe489bc994d450dca10
aditia <aditia017@gmail.com>
2021-08-05 07:13:14 +0700

    Add problem #6 - #10

--- JavaScript/OOP.js
+++ JavaScript/OOP.js
@@ -60,3 +60,84 @@ function Dog() {
   this.color = "white";
   this.numLegs = 4;
 }
+
+// 6. Use a Constructor to Create Objects
+/*
+Use the Dog constructor from the last lesson
+to create a new instance of Dog, assigning it to a variable hound.
+*/
+
+function Dog() {
+  this.name = "Rupert";
+  this.color = "brown";
+  this.numLegs = 4;
+}
+// Only change code below this line
+let hound = new Dog();
+
+// 7. Extend Constructors to Receive Arguments
+/*
+Create another Dog constructor.
+This time, set it up to take the parameters name and color,
+and have the property numLegs fixed at 4.
+Then create a new Dog saved in a variable terrier.
+Pass it two strings as arguments for the name and color properties.
+*/
+
+function Dog(name, color) {
+ this.name = name;
+ this.color = color;
+ this.numLegs = 4;
+}
+
+let terrier = new Dog("Spot", "brown");
+
+// 8. Verify an Object's Constructor with instanceof
+/*
+Create a new instance of the House constructor,
+calling it myHouse and passing a number of bedrooms.
+Then, use instanceof to verify that it is an instance of House.
+*/
+
+function House(numBedrooms) {
+  this.numBedrooms = numBedrooms;
+}
+
+// Only change code below this line
+
+let myHouse = new House(2);
+myHouse instanceof House;
+
+// 9. Understand Own Properties
+/*
+Add the own properties of canary to the array ownProps.
+*/
+
+function Bird(name) {
+  this.name = name;
+  this.numLegs = 2;
+}
+
+let canary = new Bird("Tweety");
+let ownProps = [];
+// Only change code below this line
+
+for (let property in canary) {
+  if (canary.hasOwnProperty(property)) {
+    ownProps.push(property);
+  }
+}
+
+// 10. Use Prototype Properties to Reduce Duplicate Code
+/*
+Add a numLegs property to the prototype of Dog
+*/
+
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+// Only change code above this line
+let beagle = new Dog("Snoopy");
