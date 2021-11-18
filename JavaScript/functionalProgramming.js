57f9f186ec3225adbcc34f2bf18d9d7f63db5bf6
aditia <aditia017@gmail.com>
2021-11-19 05:48:00 +0900

    Add and completed problem #19. BAM!

--- JavaScript/functionalProgramming.js
+++ JavaScript/functionalProgramming.js
@@ -798,3 +798,21 @@ function nonMutatingSort(arr) {
 }
 
 console.log(nonMutatingSort(globalArray));
+
+// 19. Split a String into an Array Using the split Method
+
+/*
+Use the split method inside the splitify function to split str into an array of words.
+The function should return the array.
+Note that the words are not always separated by spaces,
+and the array should not contain punctuation.
+*/
+
+function splitify(str) {
+  // Only change code below this line
+  const newStr = str.split(/\W/); // \W = not word
+  return newStr;
+  // Only change code above this line
+}
+
+console.log(splitify("Hello World,I-am code"));
