46aab76053655e49170c7657c5a8672b2a9379b8
aditia <aditia017@gmail.com>
2021-11-28 07:08:00 +0900

    Still stuck on #25

--- JavaScript/functionalProgramming.js
+++ JavaScript/functionalProgramming.js
@@ -911,3 +911,20 @@ function checkPositive(arr) {
 checkPositive([1, 2, 3, -4, 5]);
 
 // 25. Introduction to Currying and Partial Application
+
+/*
+Fill in the body of the add function so it uses currying
+to add parameters x, y, and z.
+*/
+
+function add(x) {
+  // Only change code below this line
+  function add(x) {
+    return function(y, z) {
+        return x + y + z;
+    }
+  }
+  // Only change code above this line
+}
+
+console.log(add(10)(20)(30));
